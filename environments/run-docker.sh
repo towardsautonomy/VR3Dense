933e1b98914f8464f2c380b3fa6d1e89e55b035a
towardsautonomy <shubham@TowardsAutonomy.com>
2021-08-18 12:28:37 -0700

    update docker run script to enable x11 socket forwarding

--- environments/run-docker.sh
+++ environments/run-docker.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
+## run the docker container with X11 socket forwarding
 MNT_DIR=/vr3dense
-sudo docker run -it --rm -v ${PWD}/../:${MNT_DIR} -v /media:/media --gpus all towardsautonomy/environments:cuda-11.2-pytorch-1.8.1-tf-2.4.1-opencv
+# create a xauth file with access permission
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f /tmp/.docker.xauth-n nmerge -
+sudo docker run -it --rm -e DISPLAY=unix$DISPLAY                     \
+                         -v ${PWD}/../:${MNT_DIR}                    \
+                         -v /media:/media                            \
+                         -v /tmp/.X11-unix:/tmp/.X11-unix            \
+                         -v /tmp/.docker.xauth:/tmp/.docker.xauth:rw \
+                         -e XAUTHORITY=/tmp/.docker.xauth            \
+                         --net=host                                  \
+                         --gpus all towardsautonomy/environments:cuda-11.2-pytorch-1.8.1-tf-2.4.1-opencv
